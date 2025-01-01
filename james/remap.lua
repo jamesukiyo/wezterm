e83fde0d4f1b08d6aaf53ce5e12eda86083bddc8
James Plummer <154843404+jamesukiyo@users.noreply.github.com>
2025-01-01 17:11:10 +0100

    switch alt to f24 to work with WM better

--- james/remap.lua
+++ james/remap.lua
@@ -18,21 +18,21 @@ local module = {}
 
 function module.apply_to_config(config)
 
-    config.leader = { key = 'w', mods = 'ALT' }
+    config.leader = { key = 'w', mods = 'F24' }
 
     config.keys = {
-        { key = 'h', mods = 'ALT', action = act.ActivatePaneDirection('Prev') },
-        { key = 'l', mods = 'ALT', action = act.ActivatePaneDirection('Next') },
+        { key = 'h', mods = 'F24', action = act.ActivatePaneDirection('Prev') },
+        { key = 'l', mods = 'F24', action = act.ActivatePaneDirection('Next') },
 
-        { key = 'o', mods = 'ALT', action = act.ShowTabNavigator },
+        { key = 'o', mods = 'F24', action = act.ShowTabNavigator },
 
-        { key = 't', mods = 'ALT', action = act.SpawnTab('CurrentPaneDomain') },
-        { key = 'q', mods = 'ALT', action = act.CloseCurrentTab({ confirm = true }) },
+        { key = 't', mods = 'F24', action = act.SpawnTab('CurrentPaneDomain') },
+        { key = 'q', mods = 'F24', action = act.CloseCurrentTab({ confirm = true }) },
 
-        { key = 'Q', mods = 'ALT', action = act.CloseCurrentPane({ confirm = true }) },
-        { key = 'L', mods = 'ALT', action = act.SplitPane({ direction = 'Right' }) },
-        { key = 'J', mods = 'ALT', action = act.SplitPane({ direction = 'Down' }) },
-        { key = 's', mods = 'ALT', action = act.EmitEvent('toggle-tabbar') },
+        { key = 'Q', mods = 'F24', action = act.CloseCurrentPane({ confirm = true }) },
+        { key = 'L', mods = 'F24', action = act.SplitPane({ direction = 'Right' }) },
+        { key = 'J', mods = 'F24', action = act.SplitPane({ direction = 'Down' }) },
+        { key = 's', mods = 'F24', action = act.EmitEvent('toggle-tabbar') },
         { key = 'h', mods = 'LEADER', action = act.AdjustPaneSize({ 'Left', 20 }) },
         { key = 'l', mods = 'LEADER', action = act.AdjustPaneSize({ 'Right', 20 }) },
         { key = 'j', mods = 'LEADER', action = act.AdjustPaneSize({ 'Down', 20 }) },
@@ -40,10 +40,10 @@ function module.apply_to_config(config)
     }
 
     for i = 1, 8 do
-        -- ALT + number to activate that tab
+        -- F24 + number to activate that tab
         table.insert(config.keys, {
             key = tostring(i),
-            mods = 'ALT',
+            mods = 'F24',
             action = act.ActivateTab(i - 1),
         })
     end
