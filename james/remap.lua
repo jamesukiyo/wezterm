149fa9c2c5553b8d6e054f6b385895e7557c83a0
James Plummer <154843404+jamesukiyo@users.noreply.github.com>
2025-01-02 21:11:45 +0100

    change leader to work better with komorebi

--- james/remap.lua
+++ james/remap.lua
@@ -18,32 +18,32 @@ local module = {}
 
 function module.apply_to_config(config)
 
-    config.leader = { key = 'w', mods = 'F24' }
+    config.leader = { key = 'w', mods = 'ALT', timeout_milliseconds = 5000 }
 
     config.keys = {
-        { key = 'h', mods = 'F24', action = act.ActivatePaneDirection('Prev') },
-        { key = 'l', mods = 'F24', action = act.ActivatePaneDirection('Next') },
+        { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Prev') },
+        { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection('Next') },
 
-        { key = 'o', mods = 'F24', action = act.ShowTabNavigator },
+        { key = 'o', mods = 'LEADER', action = act.ShowTabNavigator },
 
-        { key = 't', mods = 'F24', action = act.SpawnTab('CurrentPaneDomain') },
-        { key = 'q', mods = 'F24', action = act.CloseCurrentTab({ confirm = true }) },
+        { key = 't', mods = 'LEADER', action = act.SpawnTab('CurrentPaneDomain') },
+        { key = 'q', mods = 'LEADER', action = act.CloseCurrentTab({ confirm = true }) },
 
-        { key = 'Q', mods = 'F24', action = act.CloseCurrentPane({ confirm = true }) },
-        { key = 'L', mods = 'F24', action = act.SplitPane({ direction = 'Right' }) },
-        { key = 'J', mods = 'F24', action = act.SplitPane({ direction = 'Down' }) },
-        { key = 's', mods = 'F24', action = act.EmitEvent('toggle-tabbar') },
-        { key = 'h', mods = 'LEADER', action = act.AdjustPaneSize({ 'Left', 20 }) },
-        { key = 'l', mods = 'LEADER', action = act.AdjustPaneSize({ 'Right', 20 }) },
-        { key = 'j', mods = 'LEADER', action = act.AdjustPaneSize({ 'Down', 20 }) },
-        { key = 'k', mods = 'LEADER', action = act.AdjustPaneSize({ 'Up', 20 }) },
+        { key = 'Q', mods = 'LEADER', action = act.CloseCurrentPane({ confirm = true }) },
+        { key = 'L', mods = 'LEADER', action = act.SplitPane({ direction = 'Right' }) },
+        { key = 'J', mods = 'LEADER', action = act.SplitPane({ direction = 'Down' }) },
+        { key = 's', mods = 'LEADER', action = act.EmitEvent('toggle-tabbar') },
+        { key = 'h', mods = 'LEADER|CTRL', action = act.AdjustPaneSize({ 'Left', 20 }) },
+        { key = 'l', mods = 'LEADER|CTRL', action = act.AdjustPaneSize({ 'Right', 20 }) },
+        { key = 'j', mods = 'LEADER|CTRL', action = act.AdjustPaneSize({ 'Down', 20 }) },
+        { key = 'k', mods = 'LEADER|CTRL', action = act.AdjustPaneSize({ 'Up', 20 }) },
     }
 
     for i = 1, 8 do
-        -- F24 + number to activate that tab
+        -- LEADER + number to activate that tab
         table.insert(config.keys, {
             key = tostring(i),
-            mods = 'F24',
+            mods = 'LEADER',
             action = act.ActivateTab(i - 1),
         })
     end
